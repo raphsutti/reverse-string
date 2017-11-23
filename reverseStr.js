b0a7d601cd65299fe578146862fe4011ae8aa786
raphsutti <neoster9@gmail.com>
2017-11-23 19:49:36 +1100

    added reverStr

---
+++ reverseStr.js
@@ -0,0 +1,10 @@
+function reverseString(str) {
+  strArr = [];
+  for (var i =str.length; i>=0; i--) {
+    strArr.push(str[i]);
+  }
+  var str = strArr.join("");
+  return str;
+}
+
+reverseString("hello");
